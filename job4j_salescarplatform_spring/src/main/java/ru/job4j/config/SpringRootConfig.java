39a96c4118569b12686c97c1843411a5e0822404
RomanMozhaev <roman.mozhaev@gmail.com>
2020-03-24 15:06:34 +0000

    clean project

--- job4j_salescarplatform_spring/src/main/java/ru/job4j/config/SpringRootConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.job4j")
-public class SpringRootConfig {
-}
